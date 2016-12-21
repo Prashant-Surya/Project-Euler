805c078763fa4b9fa92a4be08a13b741294376a1
Surya Prashanth <prashantsurya@ymail.com>
2016-12-22 00:50:37 +0530

    solved 18th

--- 18.py
+++ 18.py
@@ -7,8 +7,8 @@ for line in f:
     data.append(row)
 
 f.close()
-#print data
-data = [[3], [7,4], [2,4,6], [8,5,9,3]]
+print data
+#data = [[3], [7,4], [2,4,6], [8,5,9,3]]
 nums = []
 i = 0
 """
@@ -40,7 +40,7 @@ for row in range(3, len(data)):
             temp_row.append(j+cur[j])
             temp_row.append(j+cur[j+1])
         new.append(temp_row)
-"""
+
 out = []
 import ipdb; ipdb.set_trace()
 for row in range(0, len(data)):
@@ -57,8 +57,47 @@ for row in range(0, len(data)):
                 new.append(num+current[l+1])
             new_out.append(new)
         out = new_out
+"""
+out = []
+
+#import ipdb; ipdb.set_trace()
+for row in range(0, len(data)):
+    current = data[row]
+    max_len = len(out)
+    if not out:
+        out = [current]
+    else:
+        temp = []
+        for index in range(0, len(current)):
+            cur_ele = current[index]
+            print row, index, cur_ele
+            if index == 0:
+                temp.append([out[0][0]+cur_ele])
+            elif index == len(current)-1:
+                temp.append([out[-1][0]+cur_ele])
+            else:
+                #temp.append(current[i]+out[i])
+                #temp.append(current[i]+out[i-1])
+                temp_store = 0
+                temp_list = []
+                for k in out[index]:
+                    temp_store = cur_ele + k
+                    temp_list.append(temp_store)
+                temp_store = 0
+                for k in out[index-1]:
+                    temp_store = cur_ele + k
+                    temp_list.append(temp_store)
+                temp.append(temp_list)
+        out = temp
+        
 
 #print total
 #print nums
 
-print out
+M = 0
+
+for i in out:
+    if max(i)>M:
+        M = max(i)
+
+print M
